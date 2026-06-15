21c0c9fafe4771d0f378a427ab6e86228c0fa6f7
Thuong Hoang <thuonghoang@Thuongs-MacBook-Pro.local>
2026-06-15 18:58:36 -0500

    initial commit with rearranging files and implementation of half-precision, low precision and lora

---
+++ homework1/advances-deep-learning/homework/lora.py
@@ -0,0 +1,87 @@
+import math
+from pathlib import Path
+
+import torch
+
+from .bignet import BIGNET_DIM, LayerNorm  # noqa: F401
+from .half_precision import HalfLinear
+
+
+class LoRALinear(HalfLinear):
+    lora_a: torch.nn.Module
+    lora_b: torch.nn.Module
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        lora_dim: int,
+        bias: bool = True,
+    ) -> None:
+        """
+        Implement the LoRALinear layer as described in the homework
+
+        Hint: You can use the HalfLinear class as a parent class (it makes load_state_dict easier, names match)
+        Hint: Remember to initialize the weights of the lora layers
+        Hint: Make sure the linear layers are not trainable, but the LoRA layers are
+        """
+        super().__init__(in_features, out_features, bias)
+        # ensuring linear layers are not trainable
+        self.requires_grad_(False)
+        # LoRA layers
+        self.lora_a = torch.nn.Linear(in_features, lora_dim, bias=False, dtype=torch.float32)
+        self.lora_b = torch.nn.Linear(lora_dim, out_features, bias=False, dtype=torch.float32)
+        # Initialize LoRA layers
+        torch.nn.init.kaiming_uniform_(self.lora_a.weight, a=math.sqrt(5))
+        torch.nn.init.zeros_(self.lora_b.weight)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        original_dtype = x.dtype
+        x_converted = x.to(torch.float16)
+        linear_output = super().forward(x_converted)
+        lora_output = self.lora_b(self.lora_a(x))
+        return (linear_output.to(original_dtype) + lora_output.to(original_dtype))
+
+
+class LoraBigNet(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, channels: int, lora_dim: int):
+            super().__init__()
+            self.model = torch.nn.Sequential(
+                LoRALinear(channels, channels, lora_dim),
+                torch.nn.ReLU(),
+                LoRALinear(channels, channels, lora_dim),
+                torch.nn.ReLU(),
+                LoRALinear(channels, channels, lora_dim),
+            )
+
+        def forward(self, x: torch.Tensor):
+            return self.model(x) + x
+
+    def __init__(self, lora_dim: int = 32):
+        super().__init__()
+
+        self.model = torch.nn.Sequential(
+            self.Block(BIGNET_DIM, lora_dim),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM, lora_dim),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM, lora_dim),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM, lora_dim),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM, lora_dim),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM, lora_dim),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+
+def load(path: Path | None) -> LoraBigNet:
+    # Since we have additional layers, we need to set strict=False in load_state_dict
+    net = LoraBigNet()
+    if path is not None:
+        net.load_state_dict(torch.load(path, weights_only=True), strict=False)
+    return net
